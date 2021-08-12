7d4407ce8125686ff3cf6be38aee57a1fa8698ce
Jackk-Doe <dodo-stv@hotmail.com>
2021-08-13 09:35:21 +1200

    Ship Movement
    Add Ship movement, Key reading method

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float speedGainPerSecond = 0.2f;
+
+    [SerializeField] private float turnSpeed = 100f;
+    private int steerValue = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("a")) 
+        {
+            transform.Rotate(0f, 0f, -steerValue * turnSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey("d")) 
+        {
+            transform.Rotate(0f, 0f, steerValue * turnSpeed * Time.deltaTime);
+        }
+
+        speed += speedGainPerSecond * Time.deltaTime;
+        transform.Translate(Vector3.down * speed * Time.deltaTime);
+    }
+}
