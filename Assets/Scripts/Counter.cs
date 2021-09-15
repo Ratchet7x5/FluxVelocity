e1ee51622d1f44c3050a518968889d70ae82c201
Ratchet7x5 <36789694+Ratchet7x5@users.noreply.github.com>
2021-09-15 17:09:23 +1200

    Squashed commit of the following:
    commit 3405c1b855cd43683091b930f9b91f73c6480fb9
    Author: Lachlan Pearce <Lachlanrp1208@gmail.com>
    Date:   Wed Sep 15 17:02:33 2021 +1200
    
        tutorial/Lachlan

---
+++ Assets/Scripts/Counter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountDownTimer : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingtime = 5f;
+
+    private void Start()
+    {
+        currentTime = startingtime;
+    }
+
+    private void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        //countDownText.text = currentTime.ToString();
+    }
+}
