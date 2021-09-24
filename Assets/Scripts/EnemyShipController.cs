35f80a71fa32e65bcae25aca24268e9894991135
Jackk-Doe <dodo-stv@hotmail.com>
2021-09-24 22:09:47 +1200

    Added Comments to AI script

--- Assets/Scripts/EnemyShipController.cs
+++ Assets/Scripts/EnemyShipController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/* EnemyShip Control script */
 public class EnemyShipController : MonoBehaviour
 {
     [SerializeField] private float initialSpeed = 5f;
@@ -9,8 +10,10 @@ public class EnemyShipController : MonoBehaviour
     [SerializeField] private float maxSpeed = 20f;
     [SerializeField] private List<GameObject> wayPoints;
 
-    private Vector3 headingPosition;
+    // Ship heading direction
+    private Vector3 currentHeadingWayPoint;
 
+    // Use to track current index in wayPoints List
     private int wayPointIndex;
 
     private float currentSpeed = 0;
@@ -19,7 +22,8 @@ public class EnemyShipController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        headingPosition = wayPoints[0].transform.position;
+        // Set headingDirection to first index in a List
+        currentHeadingWayPoint = wayPoints[0].transform.position;
         wayPointIndex = 0;
         currentSpeed = initialSpeed;
     }
@@ -27,19 +31,25 @@ public class EnemyShipController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Pass these values to methods below
         float forwardSpeed = currentSpeed;
         float turnAmount = 0;
 
-        Vector3 directionToMovePosition = (headingPosition - transform.position).normalized;
+        // Get a heading direction to move to
+        Vector3 directionToMoveTo = (currentHeadingWayPoint - transform.position).normalized;
 
-        float angleToDirection = Vector3.SignedAngle(transform.forward, directionToMovePosition, Vector3.up);
+        // Get a direction angle
+        float angleToDirection = Vector3.SignedAngle(transform.forward, directionToMoveTo, Vector3.up);
 
+        // minimize the above value
         angleToDirection  *= -1;
         angleToDirection /= 45;
 
+        // Move Right
         if (angleToDirection > 1) {
             turnAmount = steerValue;
         }
+        // Move Left
         else if (angleToDirection < -1 ) {
             turnAmount = -steerValue;
         }
@@ -47,6 +57,7 @@ public class EnemyShipController : MonoBehaviour
         // Accelerate
         currentSpeed = (currentSpeed == maxSpeed) ? (maxSpeed) : (currentSpeed + acceleration * Time.deltaTime);
 
+        // For Debugging
         // Debug.Log(headingPosition);
         // Debug.Log(angleToDirection);
 
@@ -54,24 +65,27 @@ public class EnemyShipController : MonoBehaviour
         LeftRightMovement(turnAmount);
     }
 
+    // Moving Forward method
     private void ForwardMovement(float forwardInput) {
         transform.Translate(Vector3.forward * forwardInput * Time.deltaTime);
     }
 
+    // Moving Left Right method
     private void LeftRightMovement(float steerInput) {
         transform.Rotate(0f, -steerInput * 100f * Time.deltaTime, 0f);
     }
 
+    // Update new heading WayPoint
     private void UpdateNewWayPoint() {
         if (wayPointIndex == wayPoints.Count - 1) {
             wayPointIndex = -1;
         }
-        headingPosition = wayPoints[++wayPointIndex].transform.position;
+        currentHeadingWayPoint = wayPoints[++wayPointIndex].transform.position;
     }
 
+    // When reached the current heading waypoint, update a new waypoint
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("WayPoint")) {
-            Debug.Log("Hitted!");
             UpdateNewWayPoint();
         }
     }
