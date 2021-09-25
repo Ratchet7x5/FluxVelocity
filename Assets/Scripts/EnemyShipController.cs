b96cd3b4f8b5e7978c8fe080220cc699cf9666c0
Jackk-Doe <dodo-stv@hotmail.com>
2021-09-25 16:49:34 +1200

    Updated PathFinding Mech & AI
    - Updated PathFinding Mechanic
    - Created PathFinding script to use in Editor
    - Updated AI script

--- Assets/Scripts/EnemyShipController.cs
+++ Assets/Scripts/EnemyShipController.cs
@@ -8,9 +8,12 @@ public class EnemyShipController : MonoBehaviour
     [SerializeField] private float initialSpeed = 5f;
     [SerializeField] private float acceleration = 0.2f;
     [SerializeField] private float maxSpeed = 20f;
-    [SerializeField] private List<GameObject> wayPoints;
+    [SerializeField] private GameObject getWayPointPath;
 
-    // Ship heading direction
+    // Store all Way points in this List
+    private List<Transform> wayPoints = new List<Transform>();
+
+    // Ship heading direction (mark waypoint)
     private Vector3 currentHeadingWayPoint;
 
     // Use to track current index in wayPoints List
@@ -22,6 +25,19 @@ public class EnemyShipController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Get items (way points) from attached object's children
+        Transform[] rawInfo = getWayPointPath.GetComponentsInChildren<Transform>();
+
+        // Print out, For Debugging (for ease)
+        // for (int i = 0; i < rawInfo.Length; i++) {
+        //     Debug.Log(rawInfo[i]);
+        // }
+
+        // Add Way Points to inner List
+        for (int i = 1; i < rawInfo.Length; i++) {
+            wayPoints.Add(rawInfo[i]);
+        }
+
         // Set headingDirection to first index in a List
         currentHeadingWayPoint = wayPoints[0].transform.position;
         wayPointIndex = 0;
@@ -31,62 +47,82 @@ public class EnemyShipController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Pass these values to methods below
+        // Pass these values to ForwardMovement() & LeftRightMovement()
         float forwardSpeed = currentSpeed;
         float turnAmount = 0;
 
         // Get a heading direction to move to
         Vector3 directionToMoveTo = (currentHeadingWayPoint - transform.position).normalized;
 
-        // Get a direction angle
+        // Get a direction angle to move to
         float angleToDirection = Vector3.SignedAngle(transform.forward, directionToMoveTo, Vector3.up);
 
-        // minimize the above value
+        // Minimize the above variable value (for ease)
         angleToDirection  *= -1;
         angleToDirection /= 45;
 
-        // Move Right
+        /*
+        The <code>angleToDirection<code> return value between -2 to 2,
+        If returned value is less than -1 turn, turn LEFT,
+        If returned value is -1 < value < 1, goes forward,
+        If returned value is more than 1 turn, turn RIGHT,
+          */
         if (angleToDirection > 1) {
             turnAmount = steerValue;
         }
-        // Move Left
         else if (angleToDirection < -1 ) {
             turnAmount = -steerValue;
         }
 
         // Accelerate
-        currentSpeed = (currentSpeed == maxSpeed) ? (maxSpeed) : (currentSpeed + acceleration * Time.deltaTime);
+        currentSpeed = (currentSpeed >= maxSpeed) ? (maxSpeed) : (currentSpeed + acceleration * Time.deltaTime);
 
         // For Debugging
         // Debug.Log(headingPosition);
         // Debug.Log(angleToDirection);
 
-        ForwardMovement(forwardSpeed);
-        LeftRightMovement(turnAmount);
+        ForwardMoving(forwardSpeed);
+        LeftRightMoving(turnAmount);
+
+        ShipFallOfTrack();
     }
 
     // Moving Forward method
-    private void ForwardMovement(float forwardInput) {
+    private void ForwardMoving(float forwardInput) {
         transform.Translate(Vector3.forward * forwardInput * Time.deltaTime);
     }
 
-    // Moving Left Right method
-    private void LeftRightMovement(float steerInput) {
+    // Moving Left-Right method
+    private void LeftRightMoving(float steerInput) {
         transform.Rotate(0f, -steerInput * 100f * Time.deltaTime, 0f);
     }
 
-    // Update new heading WayPoint
-    private void UpdateNewWayPoint() {
+    // Update (mark) new heading WayPoint
+    private void UpdateNewHeadingWayPoint() {
         if (wayPointIndex == wayPoints.Count - 1) {
             wayPointIndex = -1;
         }
-        currentHeadingWayPoint = wayPoints[++wayPointIndex].transform.position;
+        currentHeadingWayPoint = wayPoints[++wayPointIndex].position;
     }
 
-    // When reached the current heading waypoint, update a new waypoint
+    // When reached the CURRENT marked heading waypoint, update a new waypoint
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("WayPoint")) {
-            UpdateNewWayPoint();
+            UpdateNewHeadingWayPoint();
+        }
+    }
+
+    // Similar with player ship, if fall off track ( Y position < -10)
+    // Destroy gameobject
+    private void ShipFallOfTrack()
+    {
+        // Get ship's current Y position
+        float currentYPosition = transform.position.y;
+
+        // If fall off track
+        if (currentYPosition < -10)
+        {
+            Destroy(gameObject);
         }
     }
 }
---
+++ Assets/Scripts/WayPointPathHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* THIS SCRIPT ONLY RUN IN EDITOR MODE */
+public class WayPointPathHandler : MonoBehaviour
+{
+    public Color drawColor;
+
+    private List<Transform> wayPointList = new List<Transform>();
+
+    // Similar with Update(), but works only in Editor,
+    // called when scene view updated
+    private void OnDrawGizmos() {
+        
+        // Set color
+        Gizmos.color = drawColor;
+
+        // Return array of transform, from it's children
+        Transform[] pathTransform = GetComponentsInChildren<Transform>();
+
+        // Reset wayPointList in each update
+        wayPointList = new List<Transform>();
+
+        for (int i = 0; i < pathTransform.Length; i++) {
+            // Add to wayPointList,
+            // If not transform of this gameobject (parent)
+            if (pathTransform[i] != transform) {
+                wayPointList.Add(pathTransform[i]);
+            }
+        }
+
+        int size = wayPointList.Count;
+
+        for (int i = 0; i < wayPointList.Count; i++) {
+            Vector3 currentPoint = wayPointList[i].position;
+            Vector3 previousPoint = wayPointList[(size - 1 + i) % size].position;
+
+            // Draw line between two given points
+            Gizmos.DrawLine(previousPoint, currentPoint);
+            Gizmos.DrawWireSphere(currentPoint, 0.5f);
+        }
+    }
+}
